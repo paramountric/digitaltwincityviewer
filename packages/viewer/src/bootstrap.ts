4848e53de97115de6177c1631346ac51feca7cdc
andreasrudena <andreas@rudena.se>
2022-02-13 13:33:11 +0100

    chore: try to boot the app with a test bounds setting

--- packages/viewer/src/bootstrap.ts
+++ packages/viewer/src/bootstrap.ts
@@ -1,7 +1,12 @@
 import { Viewer } from './Viewer';
 
 function bootstrap() {
-  new Viewer();
+  new Viewer({
+    xMin: 0,
+    yMin: 0,
+    xMax: 2 ** 16,
+    yMax: 2 ** 16,
+  });
 }
 
 export default bootstrap;
