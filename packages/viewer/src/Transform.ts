45c61ec35c54b9f5b8b71527538cb91fa90a9d50
andreasrudena <andreas@rudena.se>
2022-02-13 13:31:59 +0100

    chore: add viewmatrix

--- packages/viewer/src/Transform.ts
+++ packages/viewer/src/Transform.ts
@@ -7,25 +7,36 @@ import { ViewerProps } from './Viewer';
 export class Transform {
   private needsUpdate = true;
   public projectionMatrix = Matrix4.IDENTITY;
+  public viewMatrix = Matrix4.IDENTITY;
   private fovy = (75 * Math.PI) / 180;
   private aspect = 1;
   private near = 0.1;
   private far = Infinity;
   constructor(viewerProps: ViewerProps) {
-    this.create();
+    this.update(viewerProps);
   }
-  update(viewerProps: ViewerProps) {
-    console.log('update transform');
+  public update(viewerProps: ViewerProps) {
+    const { xMin, yMin, xMax, yMax } = viewerProps;
+    if (!xMin || !yMin || !xMax || yMax) {
+      return;
+    }
+    // todo: check what has updated and set this.needsUpdate flag
+    this.createMatrices(viewerProps);
   }
-  create() {
+  private createMatrices(viewerProps: ViewerProps) {
     if (this.needsUpdate) {
       const { fovy, aspect, near, far } = this;
+      const { width, height, zoom } = viewerProps;
+      if (!width || !height || !zoom) {
+        return;
+      }
       this.projectionMatrix = new Matrix4().perspective({
         fovy,
         aspect,
-        near,
-        far,
+        near, // todo: find a way to calculate this from extent
+        far, // todo: find a way to calculate this from extent
       });
+      this.viewMatrix = new Matrix4();
       this.needsUpdate = false;
     }
   }
