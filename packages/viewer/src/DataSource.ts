bbf99c799217a0880cbcd33a2f5ff00b7bcced1a
andreasrudena <andreas@rudena.se>
2022-02-13 18:40:28 +0100

    chore: add layer class

--- packages/viewer/src/DataSource.ts
+++ packages/viewer/src/DataSource.ts
@@ -11,9 +11,12 @@ type Box = {
 };
 
 // provide an API to the viewer with the DataSource
-// examples: geojson, cityjson, citymodel -> the data will need to be converted into layers
+// examples: geojson, cityjson, citymodel -> the data will need to be converted into layers (use the type to determint the layers to create)
 // sources/layers in UI menu
+// note: it should be possible to feed layers directly, maybe source could optionally contain layer specs?
 export type DataSourceProps = {
+  id: string;
+  type: 'box'; // create enum
   data: Box[];
   dataUrl: string;
 };
---
+++ packages/viewer/src/Layer.ts
@@ -0,0 +1,4 @@
+// Copyright (C) 2022 Andreas Rudenå
+// Licensed under the MIT License
+
+export class Layer {}
