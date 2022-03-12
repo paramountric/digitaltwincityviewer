59b85d8b9253c2ac5f75e280d2693b2fca4574e5
andreasrudena <andreas@rudena.se>
2022-03-12 17:54:19 +0100

    chore: some polygon plotting from geojson example data

--- packages/viewer/src/GeoJsonLayer.ts
+++ packages/viewer/src/GeoJsonLayer.ts
@@ -3,26 +3,33 @@
 
 import { Model } from '@luma.gl/engine';
 import { Buffer } from '@luma.gl/webgl';
-import { FeatureCollection } from 'geojson';
+import { Matrix4 } from '@math.gl/core';
+import GL from '@luma.gl/constants';
+import { FeatureCollection, Position } from 'geojson';
 import { Transform } from './Transform';
+import { Point } from './Point';
 import { Viewer } from './Viewer';
+import { triangulate, Polygon, MultiPolygon } from './utils/polygon';
 
 export type GeoJsonLayerProps = {
   id: string;
-  featureCollection: FeatureCollection;
-  showPoints: boolean;
-  showLines: boolean;
-  showPolygons: boolean;
+  type: 'geojson';
+  data: FeatureCollection;
+  showPoints?: boolean;
+  showLines?: boolean;
+  showPolygons?: boolean;
 };
 
 type LayerAttributeData = {
-  positions: number[];
-  polygonStartPositions: number[];
+  vertices: number[];
+  indices: number[];
 };
 
 const vs = `
 attribute vec2 positions;
-attribute vec2 instancePositions;
+attribute vec3 nextPositions;
+attribute vec2 vertexPositions;
+attribute float vertexValid;
 
 uniform mat4 modelMatrix;
 uniform mat4 viewProjectionMatrix;
@@ -31,10 +38,9 @@ uniform mat4 projectionMatrix;
 uniform vec4 projectionOffset;
 
 void main() {
-  vec4 pos = vec4(positions + instancePositions, 0.0, 1.0);
-  //gl_Position = project_to_clipspace(pos) * modelMatrix;
+  vec4 pos = vec4(positions, 0., 1.0);
+  //vec3 pos = project_to_clipspace(pos) * modelMatrix;
   gl_Position = viewProjectionMatrix * modelMatrix * pos;// + projectionOffset;
-  vColor = instanceColors;
 }
 `;
 
@@ -44,65 +50,128 @@ void main() {
 }
 `;
 
-// ! note that this class is in prototype stage. Todo: inhererit from a base class, extract reusable functionality to lib
-// ? keep general comments and functionality in the Layer class even if temporarily replicated here
+// ! note that this class is a play-around class for geojson data. The point is to create layers that are specialised on city objects, not the format itself
 export class GeoJsonLayer {
+  id: string;
   gl: WebGLRenderingContext;
   transform: Transform;
   props: GeoJsonLayerProps;
   models: Model[];
+  model: Model;
   constructor(viewer: Viewer, layerProps: GeoJsonLayerProps) {
+    this.id = layerProps.id;
     this.gl = viewer.context.gl;
     this.transform = viewer.transform;
-    this.props = layerProps;
-    this.update();
+    this.update(layerProps);
+  }
+
+  // todo: move polygon functionality into polygon utils, but there are already some existing libs (math.gl, mapshaper, etc)
+  getMultiPolygonData(multiPolygon: Position[][][], data) {
+    for (const polygon of multiPolygon) {
+      this.getPolygonData(polygon, data);
+    }
+  }
+
+  // note: a polygon is multipolygon due to holes
+  getPolygonData(polygon: Polygon | MultiPolygon | Position[][], data) {
+    const [centerX, centerY] = this.transform.pointToPixelPoint(
+      Point.fromLngLat(...this.transform.cityLngLat)
+    );
+    const multiPolygon =
+      Array.isArray(polygon[0]) && Number.isFinite(polygon[0][0])
+        ? [polygon]
+        : polygon;
+    const triangulateInput = [];
+    for (const poly of multiPolygon) {
+      const points = [];
+      for (const point of poly) {
+        const [x, y] = this.transform.pointToPixelPoint(
+          Point.fromLngLat(point[0], point[1])
+        );
+        points.push([x - centerX, y - centerY]);
+      }
+      triangulateInput.push(points);
+    }
+    const { vertices, indices } = triangulate(triangulateInput as MultiPolygon);
+    for (let i = 0; i < indices.length; i++) {
+      indices[i] = indices[i] + data.indexCount;
+    }
+    data.indexCount += vertices.length / 2;
+    data.indices = data.indices.concat(indices);
+    data.vertices = data.vertices.concat(vertices);
   }
 
   generateAttributeData(
-    featureCollection: FeatureCollection
+    layerProps: GeoJsonLayerProps
   ): LayerAttributeData | null {
-    const { showPoints, showLines, showPolygons } = this.props;
-    if (!showPoints || !showLines || !showPolygons) {
+    const { showPoints, showLines, showPolygons, data } = layerProps;
+    console.log(data);
+    if (!showPoints && !showLines && !showPolygons) {
       return null;
     }
-    const attributeData: LayerAttributeData = {
-      positions: [],
-      polygonStartPositions: [],
+
+    const attributeData = {
+      vertices: [],
+      indices: [],
+      indexCount: 0,
+      starts: [],
     };
+    for (const feature of data.features) {
+      if (feature.geometry.type === 'Polygon') {
+        this.getPolygonData(feature.geometry.coordinates, attributeData);
+      } else if (feature.geometry.type === 'MultiPolygon') {
+        this.getMultiPolygonData(feature.geometry.coordinates, attributeData);
+      }
+    }
+    console.log(attributeData);
 
     return attributeData;
   }
 
-  update() {
-    const data = this.generateAttributeData(this.props.featureCollection);
+  update(layerProps: GeoJsonLayerProps) {
+    const data = this.generateAttributeData(layerProps);
     if (data) {
-      this.models = this.createModels(data);
+      this.model = this.createModel(data);
     }
   }
 
-  createModels({ positions }) {
+  createModel({ vertices, indices }) {
     const gl = this.gl;
 
     const model = new Model(gl, {
-      id: this.props.id,
+      id: 'geojson',
       vs,
       fs,
       attributes: {
-        instancePositions: [
-          new Buffer(gl, new Float32Array(positions)),
-          { divisor: 1 },
+        positions: [new Buffer(gl, new Float32Array(vertices)), { size: 2 }],
+        indices: [
+          new Buffer(gl, {
+            data: new Uint32Array(indices),
+            target: GL.ELEMENT_ARRAY_BUFFER,
+          }),
+          {
+            size: 1,
+            isIndexed: true,
+          },
         ],
       },
-      uniforms: {},
+      vertexCount: indices.length,
     });
-    return [model];
+    console.log(model);
+    return model;
   }
 
   render() {
-    if (this.models.length) {
-      for (const model of this.models) {
-        model.draw();
-      }
+    if (this.model) {
+      const modelMatrix = new Matrix4();
+      //modelMatrix.rotateZ(Math.random());
+      //modelMatrix.scale(1);
+      this.model
+        .setUniforms(this.transform.getUniforms())
+        .setUniforms({
+          modelMatrix,
+        })
+        .draw();
     }
   }
 }
