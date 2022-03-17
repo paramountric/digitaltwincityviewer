f3db9ed3b6f0b1a2155c106c104816e1e13682f5
andreasrudena <andreas@rudena.se>
2022-03-17 18:42:07 +0100

    chore: remove old class

--- packages/viewer/src/DataSource.ts
+++ packages/viewer/src/DataSource.ts
@@ -1,8 +1,6 @@
 // Copyright (C) 2022 Andreas Rudenå
 // Licensed under the MIT License
 
-import { LayerProps } from './Layer';
-
 // provide an API to the viewer with the DataSource
 // examples: geojson, cityjson, citymodel -> the data will need to be converted into layers (use the type to determine the layers to create)
 // sources/layers in UI menu
