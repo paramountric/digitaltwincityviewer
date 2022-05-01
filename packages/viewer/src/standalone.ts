5c050e66665058bcfb86ae59005817c240bb5c8e
andreasrudena <andreas@rudena.se>
2022-05-01 09:23:22 +0200

    chore: add some city settings for dev

--- packages/viewer/src/standalone.ts
+++ packages/viewer/src/standalone.ts
@@ -1,5 +1,23 @@
 import { Viewer } from './Viewer';
 
+const cities = [
+  {
+    longitude: 12.6945,
+    latitude: 56.0465,
+    name: 'Helsingborg',
+  },
+  {
+    longitude: 11.9746,
+    latitude: 57.7089,
+    name: 'Göteborg',
+  },
+  {
+    longitude: 12.9656,
+    latitude: 55.591741,
+    name: 'Malmö',
+  },
+];
+
 window.addEventListener('load', () => {
   new Viewer({});
 });
