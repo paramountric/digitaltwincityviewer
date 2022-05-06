f3eac456073dd97aa7d53bd31064ac7679fecc28
andreasrudena <andreas@rudena.se>
2022-05-06 20:29:32 +0200

    chore: file loading should be async

--- projects/citymodel-file-viewer/src/store/Store.ts
+++ projects/citymodel-file-viewer/src/store/Store.ts
@@ -1,8 +1,19 @@
 import { makeObservable, observable, action } from 'mobx';
+import { Viewer, ViewerProps } from '@dtcv/viewer';
+import { parseCityModel } from '@dtcv/citymodel';
 
 // just testing app state here temporarily with a simple counter
 export class Store {
-  public count = 0;
+  public isLoading = false;
+  public showLeftMenu = false;
+  public viewer: Viewer;
+  public constructor(viewer: Viewer) {
+    this.viewer = viewer;
+    makeObservable(this, {
+      setIsLoading: action,
+      isLoading: observable,
+    });
+  }
   public exampleFiles = [
     {
       url: 'https://digitaltwincityviewer.s3.eu-north-1.amazonaws.com/Helsingborg2021.json',
@@ -10,18 +21,45 @@ export class Store {
     },
   ];
 
-  public loadExampleFile(fileIndex: number) {
-    console.log(fileIndex);
+  public async loadExampleFile(fileIndex: number) {
+    await this.loadFile(this.exampleFiles[fileIndex].url);
   }
 
-  public constructor() {
-    makeObservable(this, {
-      increment: action,
-      count: observable,
-    });
+  public setIsLoading(isLoading: boolean) {
+    this.isLoading = isLoading;
   }
 
-  public increment() {
-    this.count++;
+  async loadFile(url: string) {
+    this.setIsLoading(true);
+    const response = await fetch(url);
+    // ! what about non-json based files? Should check the file ending
+    const json = await response.json();
+    if (json.Buildings) {
+      const { buildings, ground, modelMatrix } = parseCityModel(json);
+      this.viewer.setLayerProps(
+        'buildings-layer-polygons-lod-1',
+        {
+          data: buildings,
+          modelMatrix,
+        },
+        {
+          url,
+          isLoaded: true,
+        }
+      );
+      this.viewer.setLayerProps(
+        'ground-layer-surface-mesh',
+        {
+          data: ground,
+          modelMatrix,
+        },
+        {
+          url,
+          isLoaded: true,
+        }
+      );
+    }
+    this.setIsLoading(false);
+    this.viewer.render();
   }
 }
