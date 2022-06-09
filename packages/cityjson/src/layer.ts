8a64912519788702f70b5dc1165e21dc8801697a
andreasrudena <andreas@rudena.se>
2022-06-09 09:37:07 +0200

    chore: send in the crs

--- packages/cityjson/src/layer.ts
+++ packages/cityjson/src/layer.ts
@@ -10,6 +10,11 @@ proj4.defs(
   '+proj=utm +zone=33 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs'
 );
 
+proj4.defs(
+  'EPSG:31256',
+  '+proj=tmerc +lat_0=0 +lon_0=16.33333333333333 +k=1 +x_0=0 +y_0=-5000000 +ellps=bessel +towgs84=577.326,90.129,463.919,5.137,1.474,5.297,2.4232 +units=m +no_defs'
+);
+
 export function getModelMatrix(extent, addZ?: number) {
   const min = [extent[0], extent[1], extent[2]] as vec3;
   const max = [extent[3], extent[4], extent[5]] as vec3;
@@ -34,10 +39,14 @@ export function projectCoordinate(x, y, fromProj = 'EPSG:3008') {
   return proj4(fromProj, 'EPSG:3857', [x, y]);
 }
 
-export function projectVertices(vertices) {
+export function projectVertices(vertices, fromProj?) {
   const projectedVertices = [];
   for (const vertex of vertices) {
-    const projectedCoordinate = projectCoordinate(vertex[0], vertex[1]);
+    const projectedCoordinate = projectCoordinate(
+      vertex[0],
+      vertex[1],
+      fromProj
+    );
     projectedVertices.push([
       projectedCoordinate[0],
       projectedCoordinate[1],
@@ -47,9 +56,9 @@ export function projectVertices(vertices) {
   return projectedVertices;
 }
 
-export function projectExtent(extent) {
-  const projectedExtentMin = projectCoordinate(extent[0], extent[1]);
-  const projectedExtentMax = projectCoordinate(extent[3], extent[4]);
+export function projectExtent(extent, fromProj?) {
+  const projectedExtentMin = projectCoordinate(extent[0], extent[1], fromProj);
+  const projectedExtentMax = projectCoordinate(extent[3], extent[4], fromProj);
   return [...projectedExtentMin, extent[2], ...projectedExtentMax, extent[5]];
 }
 
