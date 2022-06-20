1014c76e7021417106f2291df815768f9ea1a23b
andreasrudena <andreas@rudena.se>
2022-06-20 14:12:13 +0200

    chore: add some test files

--- projects/dtcc-cfd-vertical/src/store/Store.ts
+++ projects/dtcc-cfd-vertical/src/store/Store.ts
@@ -1,11 +1,13 @@
 import { makeObservable, observable, action } from 'mobx';
-import { Viewer, ViewerProps } from '@dtcv/viewer';
+import { Viewer, ViewerProps, UpdateLayerProps } from '@dtcv/viewer';
 import { parseCityModel } from '@dtcv/citymodel';
 
-console.log(process.env.NODE_ENV);
+const env = process.env.NODE_ENV;
 const websocketUrl =
-  process.env.NODE_ENV === 'production'
+  env === 'production'
     ? 'wss://pmtric.com/dtcv/ws'
+    : env === 'localhost'
+    ? 'ws://localhost:4000'
     : 'wss://pmtric-local.com/dtcv/ws';
 
 export class Store {
@@ -19,7 +21,7 @@ export class Store {
       setIsLoading: action,
       isLoading: observable,
     });
-    this.connectToWebsocket();
+    this.init();
   }
 
   public setIsLoading(isLoading: boolean) {
@@ -35,54 +37,70 @@ export class Store {
     this.viewer.render();
   }
 
-  addData(json: any, url: string) {
-    if (json.Buildings) {
-      const { buildings, ground, modelMatrix } = parseCityModel(json);
-      this.viewer.setLayerProps('buildings-layer-polygons-lod-1', {
-        data: buildings,
-        modelMatrix,
-      });
-      this.viewer.setLayerState('buildings-layer-polygons-lod-1', {
-        url,
-        isLoaded: true,
-      });
-      this.viewer.setLayerProps('ground-layer-surface-mesh', {
-        data: ground,
-        modelMatrix,
-      });
-      this.viewer.setLayerState('ground-layer-surface-mesh', {
-        url,
-        isLoaded: true,
-      });
+  async init() {
+    await this.connectToWebsocket();
+    // await this.loadTestData(
+    //   'http://localhost:9000/files/HelsingborgOceanen/GroundSurface.json',
+    //   'ground-layer-surface-mesh',
+    //   'ground'
+    // );
+    // await this.loadTestData(
+    //   'http://localhost:9000/files/HelsingborgOceanen/CityModel.json',
+    //   'buildings-layer-polygons-lod-1',
+    //   'buildings'
+    // );
+  }
+
+  async loadTestData(url, layerId, key) {
+    const res = await fetch(url);
+    if (res.status !== 200) {
+      return console.warn('response status: ', res.status);
     }
+    const parsed = parseCityModel(await res.json());
+    console.log(parsed);
+    this.viewer.updateLayer({
+      layerId,
+      props: {
+        data: parsed[key].data,
+        modelMatrix: parsed[key].modelMatrix,
+        //center: parsed[key].center,
+      },
+      state: {
+        url,
+      },
+    });
   }
 
   sendMessage(topic: string, message: any) {
     this.socket.send(JSON.stringify({ topic, message }));
   }
 
-  connectToWebsocket() {
-    this.socket = new WebSocket(websocketUrl);
+  connectToWebsocket(): Promise<void> {
+    return new Promise((resolve, reject) => {
+      this.socket = new WebSocket(websocketUrl);
 
-    this.socket.addEventListener('open', () => {
-      this.sendMessage('status', 'Viewer opened socket connection');
-    });
+      this.socket.addEventListener('open', () => {
+        this.sendMessage('status', 'Viewer opened socket connection');
+        resolve();
+      });
 
-    this.socket.addEventListener('message', (event: any) => {
-      const data = JSON.parse(event.data);
-      console.log('Message from server', data);
-    });
+      this.socket.addEventListener('message', (event: any) => {
+        const data = JSON.parse(event.data);
+        console.log('Message from server', data);
+      });
 
-    this.socket.addEventListener('close', (event: any) => {
-      console.log('Socket closed, attempt to reconnect', event.reason);
-      setTimeout(() => {
-        this.connectToWebsocket();
-      }, 1000);
-    });
+      this.socket.addEventListener('close', (event: any) => {
+        console.log('Socket closed, attempt to reconnect', event.reason);
+        setTimeout(() => {
+          this.connectToWebsocket();
+        }, 1000);
+      });
 
-    this.socket.addEventListener('error', (event: any) => {
-      console.error('Socket error, closing connections', event.message);
-      this.socket.close();
+      this.socket.addEventListener('error', (event: any) => {
+        console.error('Socket error, closing connections', event.message);
+        this.socket.close();
+        reject();
+      });
     });
   }
 }
