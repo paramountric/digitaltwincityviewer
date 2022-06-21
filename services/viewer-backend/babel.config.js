22703b45ea662a2a8d4c6f549a56c380e2b83867
andreasrudena <andreas@rudena.se>
2022-06-21 19:36:15 +0200

    chore: add a viewer backend for cached layers

---
+++ services/viewer-backend/babel.config.js
@@ -0,0 +1,16 @@
+const path = require('path');
+
+const config = {
+  presets: [
+    [
+      '@babel/preset-env',
+      {
+        targets: {
+          node: 'current',
+        },
+      },
+    ],
+  ],
+};
+
+module.exports = config;
