d0d1e8a676fcce7229dc8fa4958f8a968947b711
andreasrudena <andreas@rudena.se>
2022-06-29 17:11:17 +0200

    chore: update according to new type

--- packages/cityjson/src/facility.ts
+++ packages/cityjson/src/facility.ts
@@ -32,7 +32,7 @@ export function facilityLod1Data(
   };
 
   const cityObjects = Object.values(cityJson.CityObjects).filter(
-    obj => obj.type === 'FacilityFeature'
+    obj => obj.type === 'Facility'
   );
   for (const cityObject of cityObjects) {
     const color = getColor(cityObject);
