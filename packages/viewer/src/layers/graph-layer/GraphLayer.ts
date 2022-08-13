11a6e5ce3aa9c969ee062f1e8ce5032be75635bd
andreasrudena <andreas@rudena.se>
2022-08-13 08:07:09 +0200

    chore: remove logs

--- packages/viewer/src/layers/graph-layer/GraphLayer.ts
+++ packages/viewer/src/layers/graph-layer/GraphLayer.ts
@@ -134,7 +134,6 @@ export default class GraphLayer extends CompositeLayer {
   }
 
   onDrag(info, e) {
-    console.log(info, e);
     e.preventDefault();
     info.object.x = info.coordinate[0];
     info.object.fx = info.coordinate[0];
@@ -144,7 +143,6 @@ export default class GraphLayer extends CompositeLayer {
   }
 
   onDragEnd(info, e) {
-    console.log(info, e);
     info.object.fx = null;
     info.object.fy = null;
   }
