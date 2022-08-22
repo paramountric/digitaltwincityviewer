1d4051ba8c25135a037a606799044e32768e784a
Andreas Rudenå <andreas@rudena.se>
2022-08-22 17:45:17 +0200

    refactor: remove unused

--- projects/dtcc-dod/pages/_app.tsx
+++ projects/dtcc-dod/pages/_app.tsx
@@ -1,6 +1,6 @@
 import '../styles/globals.css';
 import type {AppProps} from 'next/app';
-import {QueryClient, QueryClientProvider, useQuery} from 'react-query';
+import {QueryClient, QueryClientProvider} from 'react-query';
 
 const queryClient = new QueryClient();
 
