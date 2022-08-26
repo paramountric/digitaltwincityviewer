eba3873ac49857e0e8858682f7cb446b903d58fe
Andreas Rudenå <andreas@rudena.se>
2022-08-26 16:52:01 +0200

    chore: env vars and deploy config

--- projects/dte-digital-twin-energy/next.config.js
+++ projects/dte-digital-twin-energy/next.config.js
@@ -2,6 +2,11 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  output: 'standalone',
+  publicRuntimeConfig: {
+    domain: process.env.DOMAIN,
+    protocol: process.env.PROTOCOL,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
