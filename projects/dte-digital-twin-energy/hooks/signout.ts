4455d585326dbd7f9dba1d15ee23f067b598c7e3
Andreas Rudenå <andreas@rudena.se>
2022-08-26 15:01:53 +0200

    chore: add signout hook

---
+++ projects/dte-digital-twin-energy/hooks/signout.ts
@@ -0,0 +1,17 @@
+import {useMutation, useQueryClient} from 'react-query';
+
+export function useSignOut() {
+  const queryClient = useQueryClient();
+  const signoutUrl = '/api/signout';
+  const mutation = useMutation(async () => {
+    const res = await fetch(signoutUrl);
+    if (!res.ok) {
+      throw new Error('Sign in failed');
+    }
+    return await res.json();
+  });
+  return async () => {
+    await mutation.mutateAsync();
+    queryClient.setQueryData('user', undefined);
+  };
+}
