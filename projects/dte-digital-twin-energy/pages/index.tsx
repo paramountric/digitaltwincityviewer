a8255d84705e1a683286cc57d65ac9850aee79d7
Andreas Rudenå <andreas@rudena.se>
2022-08-30 13:23:44 +0200

    chore: set the strict mode only on gui

--- projects/dte-digital-twin-energy/pages/index.tsx
+++ projects/dte-digital-twin-energy/pages/index.tsx
@@ -6,6 +6,7 @@ import Viewport from '../components/viewport';
 import Login from '../components/login';
 import {useUserInfo} from '../hooks/userinfo';
 import {useSignOut} from '../hooks/signout';
+import React from 'react';
 
 const StartPage: NextPage = () => {
   const userInfo = useUserInfo();
@@ -15,41 +16,43 @@ const StartPage: NextPage = () => {
     <div>
       <main>
         {/* Use a wrapper for the app UI to keep the canvas fixed */}
-        <div className="absolute w-full z-50 overflow-hidden">
-          <Head>
-            <title>DTE Digital Twin Energy</title>
-            <meta
-              name="description"
-              content="DTE Digital Twin Energy project by Chalmers"
-            />
-            <link rel="icon" href="/favicon.ico" />
-          </Head>
-          <div className="shadow-sm border-b top-0 sticky z-50 bg-white">
-            <div className="flex items-center justify-between mx-4">
-              <div className="flex items-center">
-                <div className="h-12 w-32 mt-3">
-                  <Image src={logo} alt="logo of DTCC" />
-                </div>
-                <div className="text-xl ml-2 text-gray-500">
-                  Digitial Twin Energy
-                </div>
-              </div>
-              {userInfo ? (
-                <div className="flex space-x-4 items-center">
-                  <div className="text-gray-600 cursor-pointer">
-                    {userInfo.userName}
+        <React.StrictMode>
+          <div className="absolute w-full z-50 overflow-hidden">
+            <Head>
+              <title>DTE Digital Twin Energy</title>
+              <meta
+                name="description"
+                content="DTE Digital Twin Energy project by Chalmers"
+              />
+              <link rel="icon" href="/favicon.ico" />
+            </Head>
+            <div className="shadow-sm border-b top-0 sticky z-50 bg-white">
+              <div className="flex items-center justify-between mx-4">
+                <div className="flex items-center">
+                  <div className="h-12 w-32 mt-3">
+                    <Image src={logo} alt="logo of DTCC" />
                   </div>
-                  <button
-                    onClick={signOut}
-                    className="border border-gray-500 text-gray-700 bg-white rounded-md px-2 cursor-pointer hover:bg-gray-100"
-                  >
-                    Log out
-                  </button>
+                  <div className="text-xl ml-2 text-gray-500">
+                    Digitial Twin Energy
+                  </div>
                 </div>
-              ) : null}
+                {userInfo ? (
+                  <div className="flex space-x-4 items-center">
+                    <div className="text-gray-600 cursor-pointer">
+                      {userInfo.userName}
+                    </div>
+                    <button
+                      onClick={signOut}
+                      className="border border-gray-500 text-gray-700 bg-white rounded-md px-2 cursor-pointer hover:bg-gray-100"
+                    >
+                      Log out
+                    </button>
+                  </div>
+                ) : null}
+              </div>
             </div>
           </div>
-        </div>
+        </React.StrictMode>
 
         {userInfo ? <Viewport /> : <Login />}
       </main>
