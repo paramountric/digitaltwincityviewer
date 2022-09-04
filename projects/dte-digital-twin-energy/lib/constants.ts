270c4a2d78c4d7c2377cc01cf458a0d043a51287
Andreas Rudenå <andreas@rudena.se>
2022-09-04 18:11:32 +0200

    chore: separate out to const file

---
+++ projects/dte-digital-twin-energy/lib/constants.ts
@@ -0,0 +1,77 @@
+// all properties in the attribute object will be shown, labels are taken from here
+const propertyLabels = {
+  deliveredEnergy: 'Delivered energy',
+  deliveredEnergy2020: 'Delivered energy 2020',
+  deliveredEnergy2030: 'Delivered energy 2030',
+  deliveredEnergy2050: 'Delivered energy 2050',
+  primaryEnergy: 'Primary energy',
+  primaryEnergy2020: 'Primary energy 2020',
+  primaryEnergy2030: 'Primary energy 2030',
+  primaryEnergy2050: 'Primary energy 2050',
+  finalEnergy: 'Final energy',
+  finalEnergy2020: 'Final energy 2020',
+  finalEnergy2030: 'Final energy 2030',
+  finalEnergy2050: 'Final energy 2050',
+  ghgEmissions: 'GHG emissions',
+  ghgEmissions2020: 'GHG emissions 2020',
+  ghgEmissions2030: 'GHG emissions 2030',
+  ghgEmissions2050: 'GHG emissions 2050',
+  heatDemand: 'Heat demand',
+  heatDemand2020: 'Heat demand 2020',
+  heatDemand2030: 'Heat demand 2030',
+  heatDemand2050: 'Heat demand 2050',
+};
+
+// these are the units, but could be done with enum instead
+const units = {
+  deliveredEnergy: 'kWh',
+  deliveredEnergy2020: 'kWh',
+  deliveredEnergy2030: 'kWh',
+  deliveredEnergy2050: 'kWh',
+  primaryEnergy: 'kWh',
+  primaryEnergy2020: 'kWh',
+  primaryEnergy2030: 'kWh',
+  primaryEnergy2050: 'kWh',
+  finalEnergy: 'kWh',
+  finalEnergy2020: 'kWh',
+  finalEnergy2030: 'kWh',
+  finalEnergy2050: 'kWh',
+  ghgEmissions: 'kgCO2-eq.',
+  ghgEmissions2020: 'kgCO2-eq.',
+  ghgEmissions2030: 'kgCO2-eq.',
+  ghgEmissions2050: 'kgCO2-eq.',
+  heatDemand: 'kWh',
+  heatDemand2020: 'kWh',
+  heatDemand2030: 'kWh',
+  heatDemand2050: 'kWh',
+};
+
+// if needs to be rounded
+const rounding: {
+  [key: string]: number;
+} = {
+  height: 1,
+  heatedFloorArea: 0,
+  deliveredEnergy: 0,
+  deliveredEnergy2020: 0,
+  deliveredEnergy2030: 0,
+  deliveredEnergy2050: 0,
+  primaryEnergy: 0,
+  primaryEnergy2020: 0,
+  primaryEnergy2030: 0,
+  primaryEnergy2050: 0,
+  finalEnergy: 0,
+  finalEnergy2020: 0,
+  finalEnergy2030: 0,
+  finalEnergy2050: 0,
+  ghgEmissions: 0,
+  ghgEmissions2020: 0,
+  ghgEmissions2030: 0,
+  ghgEmissions2050: 0,
+  heatDemand: 0,
+  heatDemand2020: 0,
+  heatDemand2030: 0,
+  heatDemand2050: 0,
+};
+
+export {propertyLabels, units, rounding};
