876e1cf1054ea5d2b6cb41d5cfa8eaf453c87f2c
Andreas Rudenå <andreas@rudena.se>
2022-09-04 16:17:48 +0200

    chore: add right menu component

---
+++ projects/dte-digital-twin-energy/components/right-menu.tsx
@@ -0,0 +1,11 @@
+type RightMenuProps = {};
+
+const RightMenu: React.FC<RightMenuProps> = () => {
+  return (
+    <div className="absolute right-1 top-30 bg-white z-30 border border-gray-300">
+      Right menu
+    </div>
+  );
+};
+
+export default RightMenu;
