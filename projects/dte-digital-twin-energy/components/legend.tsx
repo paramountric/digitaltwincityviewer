d1a4109c54999014348aa931feee32776a23eaf4
Andreas Rudenå <andreas@rudena.se>
2022-09-04 16:17:10 +0200

    refactor: remove old style code

--- projects/dte-digital-twin-energy/components/legend.tsx
+++ projects/dte-digital-twin-energy/components/legend.tsx
@@ -10,63 +10,12 @@ type LegendProps = {};
 
 const Legend: React.FC<LegendProps> = () => {
   const {getPropertyLabel, getPropertyUnit, propertyKey} = useIndicators();
-  // complete mess, but how to style css dynamically in lit element WITHOUT changing the previous set values (in a list) <- it did not work to apply it in-line is style attribute
-  // todo: at least iterate through a range to create the colorbox classes
-  // static styles = css`
-  //   :host {
-  //     z-index: 10;
-  //     position: absolute;
-  //     background: #fff;
-  //     opacity: 1;
-  //     bottom: 300px;
-  //     right: 5px;
-  //     opacity: 0.95;
-  //     width: 200px;
-  //   }
-
-  //   :host .colorbox-1 {
-  //     padding: 10px;
-  //     background-color: var(--scale-color-1);
-  //   }
-
-  //   :host .colorbox-2 {
-  //     padding: 10px;
-  //     background-color: var(--scale-color-2);
-  //   }
-
-  //   :host .colorbox-3 {
-  //     padding: 10px;
-  //     background-color: var(--scale-color-3);
-  //   }
-
-  //   :host .colorbox-4 {
-  //     padding: 10px;
-  //     background-color: var(--scale-color-4);
-  //   }
-
-  //   :host .colorbox-5 {
-  //     padding: 10px;
-  //     background-color: var(--scale-color-5);
-  //   }
-
-  //   :host .colorbox-6 {
-  //     padding: 10px;
-  //     background-color: var(--scale-color-6);
-  //   }
-
-  //   :host .colorbox-7 {
-  //     padding: 10px;
-  //     background-color: var(--scale-color-7);
-  //   }
-  // `;
 
   const selectedProperty = propertyKey;
   const isGhg = selectedProperty === 'ghgEmissions';
   const scaleKey = isGhg ? 'buildingGhg' : 'energyDeclaration';
 
   const scaleRanges = getScaleRanges(scaleKey);
-
-  console.log(scaleRanges);
 
   return (
     <div className="absolute p-1 right-2 bottom-2 bg-white grid grid-cols-4 rounded-md border border-gray-300">
