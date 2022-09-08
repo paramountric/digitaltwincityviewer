c11824126a4d85ef9a568ffdeefde42b88b78bab
Andreas Rudenå <andreas@rudena.se>
2022-09-08 14:40:05 +0200

    chore: temp remove deps

--- packages/citymodel/src/index.ts
+++ packages/citymodel/src/index.ts
@@ -1,6 +1,6 @@
 import { vec3, mat4 } from 'gl-matrix';
-import { generateColor } from '@dtcv/indicators';
-import { convert } from '@dtcv/convert';
+// import { generateColor } from '@dtcv/indicators';
+// import { convert } from '@dtcv/convert';
 
 function parseGround(fileData) {
   const groundSurface = fileData.GroundSurface || fileData.groundSurface;
@@ -159,7 +159,7 @@ function parseBuildings(fileData, crs: string, cityXY: number[]) {
       const { x, y } = polygon[j];
       const projected = [x, y];
       if (cityXY) {
-        convert(x, y, crs, cityXY, projected);
+        //convert(x, y, crs, cityXY, projected);
       }
       // const transformed = transformCoordinate(x, y, {
       //   translate: [origin.x, origin.y],
@@ -284,14 +284,16 @@ function parseSurfaceField(fileData) {
   }
   for (let i = 0; i < values.length; i++) {
     //const color = generateColor(values[i], 4, 2);
-    const color = generateColor(
-      values[i],
-      minVal,
-      maxVal,
-      'red',
-      'blue',
-      'orange'
-    );
+    // const color = generateColor(
+    //   values[i],
+    //   minVal,
+    //   maxVal,
+    //   'red',
+    //   'blue',
+    //   'orange'
+    // );
+    // todo: bring back the generateColor function
+    const color = [0.5, 0.5, 0.5];
     colors.push(color[0] / 255, color[1] / 255, color[2] / 255, 0.6);
   }
   //console.log(minVal, maxVal);
