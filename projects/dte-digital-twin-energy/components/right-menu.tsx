d872487aae98737c62761b959993a2eb4b6f9f02
Andreas Rudenå <andreas@rudena.se>
2022-09-08 13:49:08 +0200

    chore: close button

--- projects/dte-digital-twin-energy/components/right-menu.tsx
+++ projects/dte-digital-twin-energy/components/right-menu.tsx
@@ -1,6 +1,8 @@
+import {XMarkIcon} from '@heroicons/react/20/solid';
 import BuildingFeatureGeneralDisplay from './building-feature-general-display';
 import BuildingFeatureEnergyDisplay from './building-feature-energy-display';
 import {useProtectedData} from '../hooks/data';
+import {useSelectedFeature} from '../hooks/selected-feature';
 
 type RightMenuProps = {
   selectedFeatureId: string;
@@ -8,16 +10,22 @@ type RightMenuProps = {
 
 const RightMenu: React.FC<RightMenuProps> = ({selectedFeatureId}) => {
   const {getFeature} = useProtectedData();
+  const {actions} = useSelectedFeature();
 
   const feature = getFeature(selectedFeatureId);
 
   return (
     <div className="absolute right-1 top-16 bg-white z-30 rounded-md p-2 border text-m text-gray-700 border-gray-300">
-      <div className="w-full">
-        {feature.properties?.name ||
-          feature.properties?.address ||
-          feature.properties?.uuid ||
-          'Selected building'}
+      <div className="flex justify-between w-full">
+        <div className="">
+          {feature.properties?.name ||
+            feature.properties?.address ||
+            feature.properties?.uuid ||
+            'Selected building'}
+        </div>
+        <div onClick={() => actions.setFeatureId(null)} className="ml-1">
+          <XMarkIcon className="h-5 w-5 cursor-pointer hover:bg-gray-100 rounded-md" />
+        </div>
       </div>
       <BuildingFeatureGeneralDisplay
         feature={feature}
