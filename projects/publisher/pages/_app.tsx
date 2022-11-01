4bed1169dd2859c4c1029a4ea6a282c56f600132
Andreas Rudenå <andreas@rudena.se>
2022-11-01 08:01:11 +0100

    chore: config

--- projects/publisher/pages/_app.tsx
+++ projects/publisher/pages/_app.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import {AppProps} from 'next/app';
-
-import '../styles/index.css';
+import '../styles/globals.css';
 
 function MyApp({Component, pageProps}: AppProps) {
   return <Component {...pageProps} />;
