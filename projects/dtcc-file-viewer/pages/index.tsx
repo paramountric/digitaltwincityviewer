092f82d876492604077b196fed0cb51d4f6eeb03
Andreas Rudenå <andreas@rudena.se>
2022-11-16 11:40:28 +0100

    chore: add loader

--- projects/dtcc-file-viewer/pages/index.tsx
+++ projects/dtcc-file-viewer/pages/index.tsx
@@ -3,15 +3,22 @@ import Header from '../components/header';
 import LeftPanel from '../components/left-panel';
 import LoadCityDialog from '../components/load-city-dialog';
 import UploadFileDialog from '../components/upload-file-dialog';
+import {useUi} from '../hooks/use-ui';
+import Loader from '../components/loader';
 
-const StartPage = () => (
-  <div>
-    <Header></Header>
-    <LeftPanel></LeftPanel>
-    <Viewport></Viewport>
-    <UploadFileDialog></UploadFileDialog>
-    <LoadCityDialog></LoadCityDialog>
-  </div>
-);
+const StartPage = () => {
+  const {state} = useUi();
+  console.log(state.isLoading);
+  return (
+    <div>
+      <Header></Header>
+      <LeftPanel></LeftPanel>
+      <Viewport></Viewport>
+      <UploadFileDialog></UploadFileDialog>
+      <LoadCityDialog></LoadCityDialog>
+      {state.isLoading && <Loader></Loader>}
+    </div>
+  );
+};
 
 export default StartPage;
