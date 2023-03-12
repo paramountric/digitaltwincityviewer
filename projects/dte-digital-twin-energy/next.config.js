4d3dfe0c4d837cc14b39fc304da7ddc0eafb9bca
Andreas Rudenå <andreas@rudena.se>
2023-03-12 12:41:02 +0100

    config: see if this fixes the next.js crash

--- projects/dte-digital-twin-energy/next.config.js
+++ projects/dte-digital-twin-energy/next.config.js
@@ -7,6 +7,9 @@ const nextConfig = {
     domain: process.env.DOMAIN,
     protocol: process.env.PROTOCOL,
   },
+  experimental: {
+    outputFileTracingRoot: path.join(__dirname, '../../'),
+  },
 };
 
 module.exports = nextConfig;
