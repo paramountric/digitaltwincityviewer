5e1f9b7dd9b845159775a8b8c77faa643a93c408
Andreas Rudenå <andreas@rudena.se>
2023-05-15 10:56:14 +0200

    chore: remove apps for refactoring

--- projects/dtcc-cfd-vertical/components/create-flow-3-run-tool.tsx
+++
@@ -1,5 +0,0 @@
-export type RunToolProps = {};
-
-export default function RunTool(props: RunToolProps) {
-  return <div>show progress</div>;
-}
--- projects/dtcc-file-viewer/components/loader.tsx
+++
@@ -1,9 +0,0 @@
-export default function Loader() {
-  return (
-    <div className="absolute z-50 w-full h-full">
-      <div className="relative flex justify-center items-center h-screen">
-        <div className="loader ease-linear rounded-full border-8 border-t-8 border-gray-200 h-32 w-32"></div>
-      </div>
-    </div>
-  );
-}
