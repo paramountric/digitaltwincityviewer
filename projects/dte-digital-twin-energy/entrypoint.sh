adbaa8e3cf73a8a5d1e4527d02cb8b0441435a3e
andreasrudena <andreas@rudena.se>
2023-06-09 12:45:50 +0200

    chore: update paths

--- projects/dte-digital-twin-energy/entrypoint.sh
+++ projects/dte-digital-twin-energy/entrypoint.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
+# the reason for this file is to read env vars in runtime which comes from k8s configmap
+# for next.js otherwise the env vars comes from .env.production file, etc but we cannot set values there dynamically
 # no verbose
 set +x
 # config
-envFilename='.env.production'
-nextFolder='./.next/'
+envFilename='./projects/dte-digital-twin-energy/.env.production'
+nextFolder='./projects/dte-digital-twin-energy/.next/'
 function apply_path {
   # read all config file  
   while read line; do
