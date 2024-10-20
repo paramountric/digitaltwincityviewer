6280a0dfd5cbf9b3396d7e181f54d9a44af66dcc
andreasrudena <andreas@rudena.se>
2024-10-20 12:05:23 +0200

    chore: add logic around signup

--- apps/dtcv/app/signup/page.tsx
+++ apps/dtcv/app/signup/page.tsx
@@ -26,13 +26,14 @@ export default function SignUp() {
         data: {
           name: name,
         },
+        emailRedirectTo: `${location.origin}/auth/callback`,
       },
     });
 
     if (error) {
       setError(error.message);
     } else {
-      router.push("/login");
+      router.push("/auth/check-email");
     }
 
     setLoading(false);
@@ -76,16 +77,40 @@ export default function SignUp() {
                 />
               </label>
               {error && <p className="text-red-600">{error}</p>}
-              {loading ? (
-                <p>Loading...</p>
-              ) : (
-                <button
-                  className="w-full p-3 mt-4 text-white bg-secondary border rounded-md shadow-md hover:shadow-lg hover:bg-opacity-90"
-                  type="submit"
-                >
-                  Sign Up
-                </button>
-              )}
+
+              <button
+                className="w-full p-3 mt-4 text-white bg-secondary border rounded-md shadow-md hover:shadow-lg hover:bg-opacity-90"
+                disabled={loading}
+                type="submit"
+              >
+                {loading ? (
+                  <>
+                    <svg
+                      className="animate-spin -ml-1 mr-3 h-5 w-5 text-white"
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                    >
+                      <circle
+                        className="opacity-25"
+                        cx="12"
+                        cy="12"
+                        r="10"
+                        stroke="currentColor"
+                        strokeWidth="4"
+                      ></circle>
+                      <path
+                        className="opacity-75"
+                        fill="currentColor"
+                        d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+                      ></path>
+                    </svg>
+                    Creating account...
+                  </>
+                ) : (
+                  "Create account"
+                )}
+              </button>
             </form>
           </div>
         </div>
